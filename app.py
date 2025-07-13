e698e71d22327accb3653aec1ff9a8a55a1e14c1
RosellaAM <rosella.astrain@gmail.com.com>
2025-07-12 22:08:54 -0400

    Update app.py final changes

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 # Carga el dataset de vehículos.
 vehicles = pd.read_csv("vehicles_us.csv")
 
-# Crea el titulo de la aplicación.
+# Crea el título de la aplicación.
 st.title('Análisis de Vehículos Usados')
 
 # Muestra una muestra de la base de datos en la aplicación.
@@ -17,22 +17,22 @@ if full_data:
         # Muestra dataset completo (limitado a 500 filas).
         st.dataframe(vehicles.head(500))
 
-# Muestra casillas de verificación para seleccionar la grafica a mostrar.
+# Muestra casillas de verificación para seleccionar la gráfica a mostrar.
 show_hist_price = st.checkbox('Mostrar Histograma de Precios', value=False)
 show_scatter_km_price = st.checkbox('Mostrar Gráfica de Dispersión de Kilometraje vs Precio', value=False)
 show_bar_fuel = st.checkbox('Morstrar Gráfica de Barras de Precio por Combustible', value=False)
 show_boxplot_days = st.checkbox('Mostrar Gráfica de Distribución de Días en el Mercado por Condición del Vehículo', value=False)
 show_corr = st.checkbox('Mostrar la Matriz de Correlación entre las Diferentes Variables', value=False)
 
-# Crea divisor para separar el dataframe de las graficas.
+# Crea un divisor para separar el dataframe de las gráficas.
 st.divider()
 
-# Crea histograma de precios y la acción que se realizara al picarle al checkbox.
+# Histograma de precios.
 if show_hist_price:
     # Carga el mensaje.
     st.write('***Histograma del promedio de precios de vehículos usados***')
-    # Carga slide bar con el rango de precios.
-    price_range = st.slider('Rango de Precios(USD)', 
+    # Carga deslizador con el rango de precios.
+    price_range = st.slider('Rango de Precios (USD)', 
                             min_value= int(vehicles['price'].min()), 
                             max_value= int(vehicles['price'].max()),
                             value=(5000, 50000))
@@ -46,17 +46,16 @@ if show_hist_price:
                         labels={'price' : 'Precio (USD)'}, 
                         color_discrete_sequence=['purple']
                         )
-    # Muestra el gráfico interactivo en la aplicación.
     st.plotly_chart(fig1, use_container_width=True)
     # Crea divisor entre las gráficas.
     st.divider()
 
 
-# Crea grafica de dispersión y la acción que se realizara al picarle al checkbox.
+# Gráfica de dispersión kilometraje vs precio.
 if show_scatter_km_price:
     # Carga el mensaje.
     st.write('***Gráfica de dispersión: kilometraje vs precio***')
-    st.caption('Selecciona los diferentes filtros para ver mas información sobre como afectan las condiciones el kilometraje y precio.')
+    st.caption('Selecciona los diferentes filtros para ver más información sobre cómo afectan las condiciones el kilometraje y precio.')
     # Carga pills con filtros.
     vehicles_cond_km = vehicles['condition'].unique()
     selected_condition_km = st.pills("Filtros", 
@@ -79,13 +78,12 @@ if show_scatter_km_price:
                            labels={'odometer': 'Kilometraje', 'price': 'Precio (USD)'},
                            trendline='lowess'
                            )
-    # Muestra el gráfico interactivo en la aplicación.
     st.plotly_chart(fig2, use_container_width=True)
     # Crea divisor entre las gráficas.
     st.divider()
 
 
-# Crea gráfica de barras y la acción que se realizara al picarle al checkbox.
+# Gráfica de barras de precio por tipo de combustible.
 if show_bar_fuel:
     # Carga el mensaje.
     st.write('***Gráfica de barras del precio por tipo de combustible***')
@@ -96,26 +94,31 @@ if show_bar_fuel:
                           )
     if metric == 'Promedio':
         data = mean_days = vehicles.groupby('fuel')['price'].mean().reset_index()
+        y_col = 'price'
+        title = 'Precio Promedio por Tipo de Combustible'
     elif metric == 'Mediana':
         data = median_days = vehicles.groupby('fuel')['price'].median().reset_index()
+        y_col = 'price'
+        title= 'Precio Mediano por Tipo de Combustible'
     else:
         data = count_days = vehicles['fuel'].value_counts().reset_index()
         data.columns = ['fuel', 'counts']
+        y_col = 'counts'
+        title='Conteo de Vehículos por Tipo de Combustible'
     # Crea gráfica de barras.
     fig3 = px.bar(data, 
                   x='fuel', 
                   y='price',
-                  title=f'Precio {metric} por Tipo de Combustible',
-                  labels={'fuel': 'Tipo de Combustible', 'price': 'Precio (USD)'},
+                  title=title,
+                  labels={'fuel': 'Tipo de Combustible', 'price': 'Precio (USD)' if y_col == 'price' else 'Cantidad de Vehículos'},
                   color='fuel'
                   )
-    # Muestra el gráfico interactivo en la aplicación.
     st.plotly_chart(fig3, use_container_width=True)
     # Crea divisor entre las gráficas.
     st.divider()
 
 
-# Crea gráfica boxplot.
+# Gráfica boxplot días en el mercado por condición.
 if show_boxplot_days:
     # Carga el mensaje.
     st.write('***Gráfica de distribución de los días en el mercado por su condición***')
@@ -142,15 +145,14 @@ if show_boxplot_days:
                       color='condition',
                       color_discrete_sequence=px.colors.qualitative.Pastel1
                       )
-    # Muestra el gráfico interactivo en la aplicación.
     st.plotly_chart(fig4, use_container_width=True)
     # Crea divisor entre las graficas.
     st.divider()
 
-# Crea matriz de correlación.
+# Matriz de correlación entre variables numéricas.
 if show_corr:
     # Carga mensaje.
-    st.write('***Matriz de correlación por diferentes variables***')
+    st.write('***Matriz de correlación entre diferentes variables***')
     st.caption('Selecciona las variables a relacionar.')
     # Filtra los datos por columnas numericas y exluye 'is_4wd'.
     numeric_cols = vehicles.select_dtypes(include=['int64', 'float64']).columns.tolist()
@@ -167,7 +169,7 @@ if show_corr:
     fig5 = px.imshow(corr_matrix,
                      x=selected_vars,
                      y=selected_vars,
-                     title='Matriz de Correlación sobre Variables Númericas', 
+                     title='Matriz de Correlación sobre Variables Numéricas', 
                      labels=dict(x='variable', y='variable', color='Correlación'),
                      color_continuous_scale='RdBu_r',
                      range_color=[-1, 1],
